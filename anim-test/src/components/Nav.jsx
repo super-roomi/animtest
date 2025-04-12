e7651cc91b5b853e8acce0ce6baab0b736353d1c
_roomi <roomiisdead@gmail.com>
2025-04-12 22:14:11 +0300

    v1.1 Added sandwich functionality

--- anim-test/src/components/Nav.jsx
+++ anim-test/src/components/Nav.jsx
@@ -1,18 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 // eslint-disable-next-line no-unused-vars
 import { motion } from 'motion/react'
 
+
+
 export default function nav() {
+    const [showDropdown, setDropdown] = useState(false);
 
     return (
         <motion.nav>
             <motion.div variants={{ play: { y: -100 }, end: { y: 0 } }} initial="play" animate="end" className='sticky flex justify-between px-5 py-3 bg-black mt-2.5 mx-1.5 rounded-2xl'>
-                <h1 className='text-3xl text-white pt-1'>AI-<span className='text-amber-300'>Does</span></h1>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1} stroke="currentColor" className="size-8 text-white">
+                <h1 className='text-3xl text-white'>AI-<span className='text-amber-300'>Does</span></h1>
+                <svg onClick={() => setDropdown((prev => !prev))} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1} stroke="currentColor" className="size-8 text-white cursor-pointer active:scale-110 transition-all">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
                 </svg>
+                {showDropdown && <div className='dropdown'>
+                    <ul className='text-xl text-white flex flex-row gap-23'>
+                        <li>Home</li>
+                        <li>Installation</li>
+                        <li>About</li>
+                    </ul>
+                </div>}
 
             </motion.div>
-        </motion.nav>
+        </motion.nav >
     )
 }
--- anim-test/src/components/Welcome.jsx
+++ anim-test/src/components/Welcome.jsx
@@ -43,6 +43,25 @@ export default function Welcome() {
                 <h1 className='text-3xl lg:text-5xl text-white mt-2'>Where is my data going?</h1>
                 <p className='text-xl text-white mt-2'>No where, trust me bro ;)</p>
             </motion.div>
+
+            <motion.div variants={{ hidden: { y: -100 }, show: { y: 0 }, transition: { duration: 1 } }} initial='hidden' animate='show' className='flex flex-col h-128 border m-3 rounded-xl bg-black px-4 py-1'>
+                <div className='flex flex-col items-center mt-4'>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-25 text-white">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="m3.75 13.5 10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75Z" />
+                    </svg>
+                    <p className='text-xl text-white mt-3'>Lighting Fast, get your todos in no time</p>
+
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-25 text-white mt-5">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M4.26 10.147a60.438 60.438 0 0 0-.491 6.347A48.62 48.62 0 0 1 12 20.904a48.62 48.62 0 0 1 8.232-4.41 60.46 60.46 0 0 0-.491-6.347m-15.482 0a50.636 50.636 0 0 0-2.658-.813A59.906 59.906 0 0 1 12 3.493a59.903 59.903 0 0 1 10.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.717 50.717 0 0 1 12 13.489a50.702 50.702 0 0 1 7.74-3.342M6.75 15a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Zm0 0v-3.675A55.378 55.378 0 0 1 12 8.443m-7.007 11.55A5.981 5.981 0 0 0 6.75 15.75v-1.5" />
+                    </svg>
+                    <p className='text-xl text-white mt-3'>Ideal for any use case</p>
+
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-25 text-white mt-5">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 3v1.5M4.5 8.25H3m18 0h-1.5M4.5 12H3m18 0h-1.5m-15 3.75H3m18 0h-1.5M8.25 19.5V21M12 3v1.5m0 15V21m3.75-18v1.5m0 15V21m-9-1.5h10.5a2.25 2.25 0 0 0 2.25-2.25V6.75a2.25 2.25 0 0 0-2.25-2.25H6.75A2.25 2.25 0 0 0 4.5 6.75v10.5a2.25 2.25 0 0 0 2.25 2.25Zm.75-12h9v9h-9v-9Z" />
+                    </svg>
+                    <p className='text-xl text-white mt-3'>Smart and functional</p>
+                </div>
+            </motion.div>
         </>
 
     )
